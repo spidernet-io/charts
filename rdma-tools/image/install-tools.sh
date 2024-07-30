2c364212e0931652058e09624ece46c6b9194580
weizhou.lan@daocloud.io <weizhou.lan@daocloud.io>
2024-07-29 19:57:20 -0600

    sd
    Signed-off-by: weizhou.lan@daocloud.io <weizhou.lan@daocloud.io>

--- rdma-tools/image/install-tools.sh
+++ rdma-tools/image/install-tools.sh
@@ -45,6 +45,35 @@ InstallOfed(){
   apt-get install -y --no-install-recommends  libibverbs-dev libibumad3 libibumad-dev librdmacm-dev
 }
 
+InstallEnv(){
+    chmod +x /printpaths.sh
+    # HPC-X Environment variables
+    source /opt/hpcx/hpcx-init.sh
+    hpcx_load
+    # Uncomment to stop a run early with the ENV definitions for the below section
+    # /tmp/printpaths.sh ENV && false
+    # Preserve environment variables in new login shells
+    alias install='install --owner=0 --group=0'
+    /tmp/printpaths.sh export \
+      | install --mode=644 /dev/stdin /etc/profile.d/hpcx-env.sh
+    # Preserve environment variables (except *PATH*) when sudoing
+    install -d --mode=0755 /etc/sudoers.d
+    /tmp/printpaths.sh \
+      | sed -E -e '{ \
+          # Convert NAME=value to just NAME \
+          s:^([^=]+)=.*$:\1:g ; \
+          # Filter out any variables with PATH in their names \
+          /PATH/d ; \
+          # Format them into /etc/sudoers env_keep directives \
+          s:^.*$:Defaults env_keep += "\0":g \
+        }' \
+      | install --mode=440 /dev/stdin /etc/sudoers.d/hpcx-env
+    # Register shared libraries with ld regardless of LD_LIBRARY_PATH
+    echo $LD_LIBRARY_PATH | tr ':' '\n' \
+      | install --mode=644 /dev/stdin /etc/ld.so.conf.d/hpcx.conf
+    rm /printpaths.sh
+    ldconfig
+}
 
 packages=(
   iproute2
@@ -82,6 +111,7 @@ InstallOfed
 apt-get install -y --no-install-recommends "${packages[@]}"
 InstallNccl
 InstallSSH
+InstallEnv
 
 apt-get purge --auto-remove
 apt-get clean
